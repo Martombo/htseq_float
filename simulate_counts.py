c2b28254034f292646c5b3c1803a3ffbd1fef168
Martino Colombo <martino.colombo8@gmail.com>
2015-04-16 11:45:29 +0200

    final results. uneven paralogs exp -> bias in distributed FC.

--- simulate_counts.py
+++ simulate_counts.py
@@ -51,6 +51,8 @@ class SimParalog:
         self.mmatches = []
         self.len_cDNA = 0
         self.num_reps = 3
+        self.paralog1 = True
+        self.even = True
 
     def set_paralogs(self, len_cDNA, mmatches):
         """set length of cDNA and its mismatches."""
@@ -70,6 +72,7 @@ class SimParalog:
     def sim_smn(self):
         """set SMN1-SMN2 cDNA length and mismatches."""
         self.set_paralogs(1536, [814, 1098])
+        self.skip_len = 500
         self.set_ranges()
 
     def get_ambig_bases_ratio(self):
@@ -88,8 +91,15 @@ class SimParalog:
         return num_unambig_reads
 
     def sim_unambig_counts(self, num_sims):
-        """simulate num_sims read positions and return num of unambiguous."""
-        rand_poss = nr.randint(1, self.len_cDNA + 1, num_sims)
+        """simulate num_sims read positions and return num of unambiguous.
+        second paralog is alternative spliced."""
+        if self.paralog1 or self.even:
+            rand_poss = nr.randint(1, self.len_cDNA + 1, num_sims)
+            self.paralog1 = False
+        else:
+            rand_poss = nr.randint(1, self.len_cDNA + 1, num_sims)
+            rand_poss = [x for x in rand_poss if x >= self.skip_len]
+            self.paralog1 = True
         return self.get_unambig_reads(rand_poss)
 
     def sim_nbinom(self, mean):
@@ -107,23 +117,28 @@ class SimParalog:
         return tot_counts, unambig_counts, ambig_counts
 
     def run_sim(self, mean1, mean2):
-        """run a simulation of 2 paralogs with mean1 and mean2."""
+        """run an uneven simulation of 2 paralogs with mean1 and mean2."""
         counts = Counts(self.sim_counts(mean1))
         counts.add_paralog(self.sim_counts(mean2))
         return counts
 
 x = SimParalog()
 x.sim_smn()
-true_fc = 2
-unambig_err = 0
-distr_err = 0
-distr_rep_err = 0
-countsA = x.run_sim(1000,2000)
-countsB = x.run_sim(2000,2000)
-unambig_fc = np.mean(countsB.unambig1 / countsA.unambig1)
-distr_fc = np.mean(countsB.distr1 / countsA.distr1)
-distr_rep_fc = np.mean(countsB.distr_rep1 / countsA.distr_rep1)
-print 'unambig1 ' + ' '.join(str(int(round(x))) for x in countsA.unambig1.tolist() + countsB.unambig1.tolist())
-print 'unambig2 ' + ' '.join(str(int(round(x))) for x in countsA.unambig2.tolist() + countsB.unambig2.tolist())
-print 'distr1 ' + ' '.join(str(int(round(x))) for x in countsA.distr1.tolist() + countsB.distr1.tolist())
-print 'distr2 ' + ' '.join(str(int(round(x))) for x in countsA.distr2.tolist() + countsB.distr2.tolist())
+for k in range(500):
+    x.even = False
+    countsA = x.run_sim(2000,1000)
+    x.even = True
+    countsB = x.run_sim(2000,2000)
+    unambig_fc1 = np.mean(countsB.unambig1 / countsA.unambig1)
+    distr_fc1 = np.mean(countsB.distr1 / countsA.distr1)
+    distr_rep_fc1 = np.mean(countsB.distr_rep1 / countsA.distr_rep1)
+    unambig_fc2 = np.mean(countsB.unambig2 / countsA.unambig2)
+    distr_fc2 = np.mean(countsB.distr2 / countsA.distr2)
+    distr_rep_fc2 = np.mean(countsB.distr_rep2 / countsA.distr_rep2)
+    print unambig_fc1, distr_fc1*0.9, unambig_fc2, distr_fc2*1.1
+#print 'unambig1 ' + ' '.join(str(int(round(x))) for x in countsA.unambig1.tolist() + countsB.unambig1.tolist())
+#print 'unambig2 ' + ' '.join(str(int(round(x))) for x in countsA.unambig2.tolist() + countsB.unambig2.tolist())
+#print 'distr1 ' + ' '.join(str(int(round(x))) for x in countsA.distr1.tolist() + countsB.distr1.tolist())
+#print 'distr2 ' + ' '.join(str(int(round(x))) for x in countsA.distr2.tolist() + countsB.distr2.tolist())
+#print 'distr_rep1 ' + ' '.join(str(int(round(x))) for x in countsA.distr_rep1.tolist() + countsB.distr_rep1.tolist())
+#print 'distr_rep2 ' + ' '.join(str(int(round(x))) for x in countsA.distr_rep2.tolist() + countsB.distr_rep2.tolist())
--- simulate_homology.sh
+++
@@ -1,1 +0,0 @@
-for k in {0..29000}; do awk -v k=$k 'BEGIN{OFS="\t"}{$4=$4+k; print}' prova_wholeLen.gtf >prova_tmp.gtf; htseq-count -a 0 -s no prova_uniqueMapped.sam prova_tmp.gtf >>output; done
